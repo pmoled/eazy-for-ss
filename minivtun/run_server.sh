505e6b9563247a8f9f7d168424e87d050c2becd7
Yang Li <liyangyijie@users.noreply.github.com>
2018-07-16 18:22:20 +0800

    Create run_server.sh

---
+++ minivtun/run_server.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# This script will be set up minivtun
+#source /koolshare/scripts/base.sh
+
+ROOT_DIR="$(cd "$(dirname $0)"; pwd)"
+TUN_CONFIG="$ROOT_DIR/mt.conf"
+
+source $TUN_CONFIG
+
+#modprobe tun
+
+echo "1 for run, 2 for stop"
+read choose
+
+if [ "x$choose" = "x1" ];then
+	if [ -f $pid_file ];then
+		echo minivtun is already running
+		exit 1
+	fi
+	
+	$ROOT_DIR/minivtun -l $server:$server_port -a $local_addr -m $mtu -n $intf -p $pid_file -e $password -d -t rc4 && \
+	sh $ROOT_DIR/server_up.sh
+else
+	if [ ! -f $pid_file ];then
+		echo minivtun is not running yet
+		exit 1
+	fi
+	
+	sh $ROOT_DIR/server_down.sh && \
+	kill -9 `cat $pid_file` && \
+	rm $pid_file
+fi
+:
